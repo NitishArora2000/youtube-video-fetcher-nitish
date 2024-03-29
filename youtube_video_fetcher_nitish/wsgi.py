b719b1e9c9a4019cd86c38a5b8fa634084792c26
Nitish Arora <61377353+NitishArora2000@users.noreply.github.com>
2024-03-29 18:40:28 +0530

    Initial project setup

---
+++ youtube_video_fetcher_nitish/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for youtube_video_fetcher_nitish project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_video_fetcher_nitish.settings')
+
+application = get_wsgi_application()
